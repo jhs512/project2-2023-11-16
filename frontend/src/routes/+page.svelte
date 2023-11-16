3e581a96a6a3ec547d5118f524b252a749da35e4
jhs512 <jangka512@gmail.com>
2023-11-16 23:20:10 +0900

    cors

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<button class="btn btn-primary">버튼</button>
+<button class="btn btn-primary" on:click={hello}>버튼</button>
+
+<script>
+    function hello() {
+        fetch('http://localhost:8010/api/v1/articles')
+            .then(res => res.json())
+            .then(res => console.log(res))
+    }
+</script>
