1f05473a3ad9ce5c4687fa3e67ba7c0dcd0c04e7
jhs512 <jangka512@gmail.com>
2023-11-16 23:29:18 +0900

    fetch

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<button class="btn btn-primary" on:click={hello}>버튼</button>
-
 <script>
-    function hello() {
+    let articles = [];
+
+    function fetchArticles() {
         fetch('http://localhost:8010/api/v1/articles')
             .then(res => res.json())
-            .then(res => console.log(res))
+            .then(res => {
+                articles = res.data.items;
+            });
     }
 </script>
+
+<button class="btn btn-primary" on:click={fetchArticles}>게시물 불러오기</button>
+
+<ul>
+    {#each articles as article}
+        <li>
+            <strong>{article.title}</strong>: {article.body}
+        </li>
+    {/each}
+</ul>
