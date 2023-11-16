98b4c58de37266d56be341a81d028d6d0b58a083
jhs512 <jangka512@gmail.com>
2023-11-16 23:32:32 +0900

    fetch 2

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,21 +1,33 @@
 <script>
-    let articles = [];
+    import {onMount} from 'svelte';
 
-    function fetchArticles() {
-        fetch('http://localhost:8010/api/v1/articles')
-            .then(res => res.json())
-            .then(res => {
-                articles = res.data.items;
-            });
+    let articles = [];
+    let error = null;
+
+    async function fetchArticles() {
+        try {
+            const response = await fetch('http://localhost:8010/api/v1/articles');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            articles = data.data.items;
+        } catch (err) {
+            error = err.message;
+        }
     }
+
+    onMount(fetchArticles);
 </script>
 
-<button class="btn btn-primary" on:click={fetchArticles}>게시물 불러오기</button>
-
-<ul>
-    {#each articles as article}
-        <li>
-            <strong>{article.title}</strong>: {article.body}
-        </li>
-    {/each}
-</ul>
+{#if error}
+    <p class="error">Error: {error}</p>
+{:else}
+    <ul>
+        {#each articles as article}
+            <li>
+                <strong>{article.title}</strong>: {article.body}
+            </li>
+        {/each}
+    </ul>
+{/if}
